f226007fbc9dfd37da547efd27c7652cd771b6eb
elroytoscano <andeltos@gmail.com>
2022-04-11 23:40:25 +0530

    added basic structure for express app

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-require('dotenv').config()
-require('express-async-errors')
-const express = require('express')
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const morgan = require('morgan');
 
-const app = express()
-const PORT = process.env.PORT
+const connectDB = require('./db');
+const homeRoute = require('./routes/home.route');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.use('/api/v1', homeRoute);
 
 const start = async () => {
-  app.listen(PORT, () => console.log(`Server is listening on PORT:${PORT}`))
-}
+  await connectDB();
+  app.listen(PORT, () => console.log(`Server is listening on PORT:${PORT}`));
+};
 
-start()
+start();
